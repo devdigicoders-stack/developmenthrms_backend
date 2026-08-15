c51cfe7e7aa78009ce4fb0ea5e258bf0dcfa2cec
Vivek Chaurasiya <chaurasiyavivek657@gmail.com>
2026-08-15 15:42:10 +0530

    Fix RoleRoutes authorization middleware to allow authorized users to create/update roles

--- route/RoleRoutes.js
+++ route/RoleRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import { createRole, getRoles, getAllRoles,getAllRolesByCompany, updateRole,getAllCompanyRoles, deleteRole, restoreRole, toggleRoleStatus } from '../controller/RoleController.js'
-import { protect, authorize } from '../middleware/authMiddleware.js'
+import { protect, authorize, hasPermission } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.post("/create", protect, authorize("super_admin","admin"), createRole)
+router.post("/create", protect, hasPermission("Create_ROLE"), createRole)
 router.get("/all/active", getRoles)
 router.get("/all/:companyId", getAllRolesByCompany)
 router.get("/all", getAllRoles)
 router.get("/allroles", protect, getAllCompanyRoles)
-router.put("/update/:id", protect, authorize("super_admin","admin"), updateRole)
-router.delete("/delete/:id", protect, authorize("super_admin","admin"), deleteRole)
+router.put("/update/:id", protect, hasPermission("UPDATE_ROLE"), updateRole)
+router.delete("/delete/:id", protect, hasPermission("DELETE_ROLE"), deleteRole)
 router.post("/restore/:id", protect, authorize("super_admin","admin"), restoreRole)
 router.patch("/toggle-status/:id", protect, authorize("super_admin","admin"), toggleRoleStatus)
 
